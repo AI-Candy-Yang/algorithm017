85a585a0b26f5156cc9c10a9a4703a378586419f
yangshuang <yangshuang@sinandata.com>
2020-10-15 14:34:54 +0800

    括号生成，二叉树的构建和最近公共祖先，递归实现

--- Week_03/week03.py
+++ Week_03/week03.py
@@ -20,7 +20,7 @@
 
 3.回溯，也是递归的思想，用一般的递归模板即可
 
-3.括号生成问题
+3.括号生成问题 使用一个栈来维护每次的状态，根据条件每次选择左括号还是右括号
 """
 #爬楼梯
 def climbStairs(n):
@@ -35,6 +35,23 @@ def climbStairs(n):
     return f3
 
 #括号生成
+def generateParenthesis(n):
+    #递归
+    stack = [('',0,0)]  #栈里面的元素分别表示当前的括号字符串，左括号的个数，右括号的个数
+    res = []
+    while stack:
+        p,left,right = stack.pop()
+        #终止条件
+        if left == right == n:
+            res.append(p)
+            continue
+        #每次只将符合条件的括号加入
+        if left < n:
+            stack.append((p + '(',left+1,right))
+        if right < n and right < left:
+            stack.append((p + ')',left,right+1))
+    #栈为空的时候退出循环得到所有的括号
+    return res
 
 #二叉树的最大深度
 class TreeNode:
@@ -111,10 +128,53 @@ class Solution:
             cur = tmp.right
         return True
 
+    # 二叉树的最近公共祖先
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        #递归终止条件 如果找到p或者是q,则直接返回
+        if not root or root.val == p.val or root.val == q.val:
+            return root
 
-#二叉树的序列化与反序列化
-#二叉树的最近公共祖先
-#从前序与中序遍历序列构造二叉树
+        #递归左右子树
+        l = self.lowestCommonAncestor(root.left,p,q)
+        r = self.lowestCommonAncestor(root.right,p,q)
+
+        #分三种情况处理当前层逻辑
+        #1.如果p,q既不再左子树，也不再右子树，则直接返回空
+        if not l and not r:return None
+        #2.p,q同时在左子树或右子树，则返回对应的左子树或右子树
+        if not l:return r
+        if not r:return l
+
+        #3.p,q分别存在于左右子树当中，则直接返回当前节点作为最近的公共祖先
+        return root
+
+    # 从前序与中序遍历序列构造二叉树
+    def buildTree(self,preorder, inorder):
+        #递归实现
+        #根据前序遍历找到根节点，然后找到中序遍历里面根节点的位置，进而可以知道左子树的长度
+        #根据前序遍历左子树的边界和中序遍历左子树的边界构建左子树
+        #根据前序遍历右子树的边界和中序遍历右子树的边界构建右子树
+        n = len(preorder)
+        hash_dict = {val:i for i,val in enumerate(inorder)}
+        def helper(preorder_left,preorder_right,inorer_left,inorder_right):
+            if preorder_left > preorder_right:
+                return None
+            #前序遍历第一个值即为根节点
+            preorder_root = preorder_left
+            #找到中序遍历对应的根节点的位置
+            inorder_root = hash_dict[preorder[preorder_root]]
+            #得到左子树的长度
+            left_subtree_size = inorder_root - inorer_left
+            #根据根节点构建树
+            root = TreeNode(preorder[preorder_root])
+            #构建左子树
+            root.left = helper(preorder_left+1,preorder_left+left_subtree_size,inorer_left,inorder_root-1)
+            #构建右子树
+            root.right = helper(preorder_left+left_subtree_size+1,preorder_right,inorder_root+1,inorder_right)
+            return root
+        return helper(0,n-1,0,n-1)
+
+
 #组合
 #全排列
 #全排列II
@@ -144,3 +204,5 @@ def subsets(nums):
 #电话号码的字母组合
 #N皇后
 
+if __name__ == '__main__':
+    print(generateParenthesis(3))
